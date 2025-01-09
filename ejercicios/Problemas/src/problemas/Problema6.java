d4e1c038e24338e22c5bd3b4d6f8ca1cf20244f0
Bryan Diaz <vicentepomas.2007@gmail.com>
2025-01-09 12:43:42 -0500

    EJERCICIO 6

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,37 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        double media = calcularMedia(informacion);
+
+        double desviacionEstandar
+                = calcularDesviacionEstandar(informacion, media);
+
+        System.out.println("Media aritmética: " + media);
+        System.out.println("Desviación estándar: " + desviacionEstandar);
+    }
+
+    public static double calcularMedia(int[] arreglo) {
+        double suma = 0;
+        double media;
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        media = suma / arreglo.length;
+        return media;
+    }
+
+    public static double calcularDesviacionEstandar(int[] arreglo, double media) {
+        double sumaDesviacionCuadrada = 0;
+        double desviacion;
+        for (int i = 0; i < arreglo.length; i++) {
+            sumaDesviacionCuadrada = sumaDesviacionCuadrada + Math.pow(arreglo[i] - media, 2);
+        }
+        desviacion = Math.sqrt(sumaDesviacionCuadrada / arreglo.length);
+        return desviacion;
+    }
+
 }
