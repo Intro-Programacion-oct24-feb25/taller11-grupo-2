8fe1998bf6cf1671d1be385ec1b631326e468736
Carlosm1103 <carlosmedinal2006@gmail.com>
2025-01-09 12:25:47 -0500

    Ejercicio Hecho

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,26 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        String mensaje = suma(informacion,informacion2);
+        System.out.println(mensaje);
+    }
     
+    public static String suma (int[][]num1,int[][]num2){
+        String cadena = "";
+      for (int i = 0; i < num1.length; i++) {
+          cadena = String.format("%s\n", cadena);
+            for (int j = 0; j < num1[i].length; j++) {
+                
+                  int suma;
+                  suma = num1[i][j] +num2[i][j];
+                    cadena = String.format("%s%d\t", cadena, suma);
+                
+            }
+        }  
+      return cadena;
+    }
 }
